085f13932220b20be9b973d4a5a0a3bbae78b830
ColdEffects <paolomanalastas815@gmail.com>
2025-09-30 18:19:40 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;600;700&display=swap"
+      rel="stylesheet"
+    />
+
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="general.css">
+</head>
+
+
+
+<div class="centered-box">
+
+  <div class="box">
+    <h1 class="h1">Welcome!</h1><br><br><br>
+    <h2> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h2>
+  </div>
+
+  <div class="box-2">
+    <h1 class="h1">Login</h1><br><br><br>
+    <form class="login" method="POST"> 
+      <input name="username" class = "textbox" placeholder="Username" required><br><br><br><br>
+      <input name="password" class = "textbox" placeholder="Password" required><br><br><br><br>
+      <button class="signin" type="submit">Sign in</button>
+    </form>
+  </div>
+</div>
+
